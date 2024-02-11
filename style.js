91da3f196580ef924792c1f3c7825c2ab74bc425
Deepanshu1026 <143159395+Deepanshu1026@users.noreply.github.com>
2024-02-11 12:45:14 +0530

    Hellow

---
+++ style.js
@@ -0,0 +1,16 @@
+
+// let pera=document.getElementsByTagName("p");
+// let pera2=document.querySelector("p");
+// let pera3=document.querySelectorAll("p")[1];
+
+
+// let divs=document.querySelector("div").children;
+// let div=document.querySelector("ul").firstElementChild;
+// console.dir(divs);
+// console.dir(div.innerHTML="<p>Helloe</p>")
+// let heading=document.querySelector("h1");
+// console.log(heading.textContent)
+
+let ele=document.querySelector("h1").innerText;
+let ele2=ele+" is a programing language";
+document.querySelector("h1").innerText=ele;
